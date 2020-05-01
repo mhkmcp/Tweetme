5215b1c5e33d65b80bbbfac31b9afc43c1ff15c8
mhkmcp <mhkmcp@yahoo.com>
2020-05-01 12:23:41 +0600

    Added User with Tweets

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.conf import settings
 import random
+
+User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
+    # users can have many tweets, but tweet must have one user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
